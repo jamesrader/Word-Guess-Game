45c02f2690e3f128cd09e94836836952ba694de2
USCjames <james.rader@cox.net>
2019-03-04 14:00:15 -0700

    Most CSS in place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,12 @@
 
 $(document).ready(function () {
 
-    var gameArray = ["test", "testing", "testingtest"];
+    var gameArray = ["songgirls", "coliseum", "testingtest"];
+    var gameObject = [{word:"song girls", image:"SongGirls.jpg", audio:"Tusk.mp3"},
+                            {word:"coliseum", image:"Coliseum.jpg", audio:"FightOn.mp3"},
+                        {word:"tommy trojan", image:"DrumMajor.jpg", audio:"Conquest.mp3"},
+                        {word:"marching band", image:"TrojanMarchingBand.jpg", audio:"TributeToTroy.mp3"},
+                        {word:"traveler", image:"Traveler.jpg", audio:"Conquest.mp3", desc:"<p>Traveler, the noble white horse that appears at all USC home football games with a Trojan warrior astride, is one of the most famous college mascots.</p><p>Traveler first made an appearance at USC football games in 1961 in the home opener versus Georgia Tech. Bob Jani, USC’s director of special events, and Eddie Tannenbaum, a junior at USC, had spotted Richard Saukko riding his white horse, Traveler I, in the 1961 Rose Parade. They persuaded Saukko to ride his white horse around the Coliseum during USC games, serving as a mascot. Ever since, whenever USC scores, the band plays “Conquest” and Traveler gallops around the Coliseum.</p>"}];
     var revealedLetters = [];
     var wrongLetters = [];
     var remGuessCounter = 5;
@@ -12,48 +17,70 @@ $(document).ready(function () {
     var randomNumber = 0;
     var word = "";
     var wordList = [];
+    var currentSong = "";
+    var audio= $("#winMusic");
+    var currentDesc = "";
 
-    $("#playAgain").hide();
+    //Hide buttons on initial launch.
+    //CSS Visibility - 'hidden' and 'visible' maintains vertical spacing.
+    $("#playAgain").css("visibility", "hidden");
+    $("#stopMusic").css("visibility", "hidden");
 
+    //Start it all
+    resetWord();
+
+    //Play Again button
     $("#playAgain").on("click", function () {
-        //alert("RESET!");
-        $("#playAgain").hide()
+        $("#playAgain").css("visibility", "hidden")
         resetWord();
     })
 
-    resetWord();
+    //Stop music button
+    $("#stopMusic").on("click", function() {
+        audio[0].pause();
+        message = "Stop THIS music?!?  OK...way to go, bruin.";
+        $("#messageArea").html(message);
+        $("#messageArea").css("visibility", "visible");
+        $("#stopMusic").css("visibility", "hidden");
+    })
 
     function resetWord() {
-        
+
         //Reset flag
-        winner=0;
+        winner = 0;
 
         //Replenish disposable word list after all words have been played
-        if (wordList.length < 1){
-            for (i=0; i<gameArray.length; i++){
-                wordList.push(gameArray[i]);
+        if (wordList.length < 1) {
+            //for (i = 0; i < gameArray.length; i++) {
+                //wordList.push(gameArray[i]);
+            //}
+            for (i=0; i < gameObject.length; i++) {
+                wordList.push(gameObject[i].word);
             }
         }
-        
+
         //Choose word from list based on a random number
         randomNumber = Math.floor(Math.random() * wordList.length);
         word = wordList[randomNumber];
-        console.log(word);
-        
+
         //Remove this word from list so it can't be played again
         wordList.splice(randomNumber, 1);
-        console.log(wordList);
 
         //Start with no letters revealed, of course
         revealedLetters = [];
         for (i = 0; i < word.length; i++) {
+            if (word[i] != " "){
             revealedLetters.push("_");
+            } else {
+                revealedLetters.push(" ");
+            }
         }
         $("#wordToGuess").html(revealedLetters);
 
         //Set initial message with instructions
         var message = "<div class='warning'>Guess any letter to begin!</div>";
         $("#messageArea").html(message);
+        $("#messageArea").css("visibility", "visible");
 
         remGuessCounter = 5;
         $("#remGuesses").html(remGuessCounter);
@@ -92,8 +119,25 @@ $(document).ready(function () {
                     winCounter++;
                     message = "WINNER!!!";
                     $("#messageArea").html(message);
+                    $("#messageArea").css("visibility", "visible");
                     $("#winCounter").html(winCounter);
-                    $("#playAgain").show();
+
+                    for (i=0; i<gameObject.length; i++){
+                        if (gameObject[i].word === word){
+                            currentSong = gameObject[i].audio;
+                            currentImage = gameObject[i].image;
+                            currentDesc = gameObject[i].desc;
+                        }
+                    }
+                    $("#winMusic").attr("src", "assets/music/" + currentSong);
+                    //$("#winMusic").attr("src", "assets/music/TributeToTroy.mp3");
+                    audio[0].play();
+                    $("#mainImage").attr("src", "assets/images/" + currentImage)
+                    $("#description").html(currentDesc);
+
+                    
+                    $("#stopMusic").css("visibility", "visible");
+                    $("#playAgain").css("visibility", "visible");
                 }
             }
 
@@ -112,6 +156,7 @@ $(document).ready(function () {
                     if (remGuessCounter === 0) {
                         message = "Sorry...you lose.";
                         $("#messageArea").html(message);
+                        $("#messageArea").css("visibility", "visible");
                         winner = -1;
                         $("#playAgain").show();
                     }
@@ -120,13 +165,15 @@ $(document).ready(function () {
                 //Reset message...unless user has won
                 if (winner === 0) {
                     var message = ""
-                    $("#messageArea").html(message);
+                    //$("#messageArea").html(message);
+                    $("#messageArea").css("visibility", "hidden");
                 }
+
             } else {
                 message = "Uhhhh...you already guessed that! Try again.";
                 $("#messageArea").html(message);
             }
             correct = 0;
-        } 
+        }
     }
 });
